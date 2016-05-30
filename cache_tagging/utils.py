462bc1eaab92d3dbe716b4166288f63d91ca1e10
Ivan <ivzak@yandex.ru>
2016-05-30 04:23:50 +0300

    UndefType

--- cache_tagging/utils.py
+++ cache_tagging/utils.py
@@ -8,8 +8,15 @@ except ImportError:
     import thread as _thread  # Python < 3.*
 
 
-class Undef(object):
-    pass
+class UndefType(object):
+
+    def __repr__(self):
+        return "Undef"
+
+    def __reduce__(self):
+        return "Undef"
+
+Undef = UndefType()
 
 
 def get_thread_id():  # TODO: Cache result in thread-safe variable
