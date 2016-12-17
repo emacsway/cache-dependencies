11676584cb1ffb9eb45c2e84c01980e717a27c3a
Ivan <ivzak@yandex.ru>
2016-12-18 01:32:10 +0200

    TransactionManagerTestCase

--- cache_dependencies/tests/test_transaction.py
+++ cache_dependencies/tests/test_transaction.py
@@ -18,7 +18,6 @@ except NameError:
 
 
 class AbstractTransactionTestCase(unittest.TestCase):
-
     def setUp(self):
         self.current_time = mock.Mock(return_value=time.time())
         self.lock = mock.Mock(spec=interfaces.IDependency)
@@ -171,3 +170,53 @@ class DummyTransactionTestCase(AbstractTransactionTestCase):
         time.sleep(1)
         self.assertAlmostEqual(self.transaction.get_end_time(), self.current_time.return_value)
         self.assertEqual(self.current_time.call_count, 2)
+
+
+class TransactionManagerTestCase(unittest.TestCase):
+    def setUp(self):
+        self.transaction = mock.Mock(spec=interfaces.ITransaction)
+        self.lock = mock.Mock(spec=interfaces.IDependency)
+        self.transaction_manager = transaction.TransactionManager(self.lock)
+
+    def test_current_none(self):
+        current_transaction = self.transaction_manager.current()
+        self.assertIsInstance(current_transaction, transaction.DummyTransaction)
+
+    def test_current_set_get(self):
+        self.transaction_manager.current(self.transaction)
+        self.assertIs(self.transaction_manager.current(), self.transaction)
+
+    def test_begin(self):
+        initial_transaction = self.transaction_manager.begin()
+        self.assertIsInstance(initial_transaction, transaction.Transaction)
+
+        save_point = self.transaction_manager.begin()
+        self.assertIsInstance(save_point, transaction.SavePoint)
+        self.assertIs(save_point.parent(), initial_transaction)
+
+        nested_save_point = self.transaction_manager.begin()
+        self.assertIsInstance(nested_save_point, transaction.SavePoint)
+        self.assertIs(nested_save_point.parent(), save_point)
+
+    def test_finish_delegate_transaction(self):
+        self.transaction_manager.current(self.transaction)
+        self.transaction_manager.finish()
+        self.transaction.finish.assert_called_once_with()
+
+    def test_finish_transaction(self):
+        self.transaction_manager.begin()
+        self.transaction_manager.finish()
+        self.assertIsInstance(self.transaction_manager.current(), transaction.DummyTransaction)
+
+    def test_finish_savepoint(self):
+        initial_transaction = self.transaction_manager.begin()
+        self.transaction_manager.begin()
+        self.transaction_manager.finish()
+        self.assertIs(self.transaction_manager.current(), initial_transaction)
+
+    def test_flush(self):
+        self.transaction_manager.begin()
+        self.transaction_manager.begin()
+        self.transaction_manager.begin()
+        self.transaction_manager.flush()
+        self.assertIsInstance(self.transaction_manager.current(), transaction.DummyTransaction)
